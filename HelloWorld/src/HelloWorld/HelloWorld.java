8eeed6b0bc876b114b4f638f50cb9952893a610a
Ricardo55010 <richi_mari.2011@hotmail.com>
2024-09-26 13:52:48 -0600

    edit:Hello world file modified to print the message on the same sentence with both print and println

--- HelloWorld/src/HelloWorld/HelloWorld.java
+++ HelloWorld/src/HelloWorld/HelloWorld.java
@@ -2,6 +2,7 @@ package HelloWorld;
 
 public class HelloWorld {
 	public static void main(String args[]){
-		System.out.println("Hello World");
+		System.out.print("Hello ");
+		System.out.println("World");
 	}
 }
