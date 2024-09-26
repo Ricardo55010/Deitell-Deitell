74192aed408b2fb7b2d626b457bfd9a4795f6331
Ricardo55010 <richi_mari.2011@hotmail.com>
2024-09-26 14:01:17 -0600

    Create: New Adding integers program

---
+++ AddingIntegers/src/AddingIntegers/AddingIntegers.java
@@ -0,0 +1,16 @@
+package AddingIntegers;
+import java.util.Scanner;
+public class AddingIntegers {
+	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+
+		int number1 = input.nextInt();
+		
+		int number2 = input.nextInt();
+		
+		int sum = number1 + number2;
+		
+		System.out.printf("Sum is %d%n", sum);
+		
+	}
+}
