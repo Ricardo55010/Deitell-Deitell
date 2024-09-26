6325468ebb79307894a883893cc827e97cfe8706
Ricardo55010 <richi_mari.2011@hotmail.com>
2024-09-26 13:55:29 -0600

    edit:Hello world file modified to print formatted hello world sentence

--- HelloWorld/src/HelloWorld/HelloWorld.java
+++ HelloWorld/src/HelloWorld/HelloWorld.java
@@ -2,7 +2,9 @@ package HelloWorld;
 
 public class HelloWorld {
 	public static void main(String args[]){
-		System.out.print("Hello ");
-		System.out.println("World");
+//		System.out.print("Hello ");
+//		System.out.println("World");
+		System.out.printf("%s%n%s%n","Hello","World");
+		
 	}
 }
